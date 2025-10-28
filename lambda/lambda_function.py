b9010827a4308ac9616f11aeccf02e8134a4c4cb
ShayneSL1 <ssluangvisa@gmail.com>
2025-10-28 02:32:22 -0400

    changed the function body, and updated python version to 3.12 from 3.13

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event,context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello, this message was updated via VSCode!!')
+        'body': json.dumps('Hello, this message was changed via VSC')
     }
